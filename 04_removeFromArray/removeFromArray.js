c147878a9d0e51a0b40dd6752a8923871551e063
Ifham-Hashir <ifham09052003@gmail.com>
2024-06-20 08:28:05 +0530

    Complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+  arr = [...new Set(arr)];
+  for(let i = 0; i < args.length; i++){
+    if(arr.includes(args[i]) === false){
+      continue;
+    }
+    const index = arr.indexOf(args[i]);
+    arr.splice(index, 1);
+  }
+  return arr;
 };
 
 // Do not edit below this line
