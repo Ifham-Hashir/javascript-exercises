c8b72c5da3dbc8b7d14714f7a4d61c88e7de83c6
Ifham-Hashir <ifham09052003@gmail.com>
2024-06-30 20:28:41 +0530

    complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  const strRev =  str.split('').reverse().join('');
+  if(str === strRev){
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
