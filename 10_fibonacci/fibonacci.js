9cfa90271eecc42a8cf19ae9f9f8deef31394433
Ifham-Hashir <ifham09052003@gmail.com>
2024-07-01 08:35:43 +0530

    Complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  num = Number(num);
+  if(num < 0){
+    return "OOPS";
+  } else if(num === 1 || num === 2){
+    return 1;
+  } else if(num === 0){
+    return 0;
+  }
 
-};
+  let fibo = 0;
+  for(let i = 2; i < num; i++){
+    fibo = (i - 1) + (i - 2);
+  }
+  return fibo;
 
+}
 // Do not edit below this line
 module.exports = fibonacci;
